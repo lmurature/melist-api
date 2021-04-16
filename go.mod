3f8970c76abfc2b30da58dcff592afe1b8642f07
lmurature <lucio.murature@mercadolibre.com>
2021-04-16 00:00:05 -0300

    changed go version and adde users service test

--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
 module github.com/lmurature/melist-api
 
-go 1.16
+go 1.12
 
 require (
 	github.com/gin-gonic/gin v1.7.1
 	github.com/mercadolibre/golang-restclient v0.0.0-20170701022150-51958130a0a0
 	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.4.0
 )
---
+++ services/users/users_service_test.go
@@ -0,0 +1,56 @@
+package users_service
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/mercadolibre/golang-restclient/rest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	rest.FlushMockups()
+}
+
+func GetUserError(t *testing.T) {
+	rest.FlushMockups()
+	headers := make(http.Header)
+	headers.Add("Authorization", "Bearer a1b2c3d4e5")
+	rest.AddMockups(&rest.Mock{
+		URL: "https://api.mercadolibre.com/users/1",
+		HTTPMethod: http.MethodGet,
+		ReqHeaders: headers,
+		RespHTTPCode: http.StatusForbidden,
+		RespBody: `{"error": "forbidden", "message": "you are not authorized to access this resource", "status": 403}`,
+	})
+
+	s := &usersService{}
+	
+	user, err := s.GetUser(1, "a1b2c3d4e5")
+
+	assert.NotNil(t, err)
+	assert.Nil(t, user)
+	assert.EqualValues(t, "you are not authorized to access this resource", err.Message())
+	assert.EqualValues(t, 403, err.Status())
+}
+
+func GetUserNoError(t *testing.T) {
+	rest.FlushMockups()
+	headers := make(http.Header)
+	headers.Add("Authorization", "Bearer a1b2c3d4e5")
+	rest.AddMockups(&rest.Mock{
+		URL: "https://api.mercadolibre.com/users/1",
+		HTTPMethod: http.MethodGet,
+		ReqHeaders: headers,
+		RespHTTPCode: http.StatusOK,
+		RespBody: `{"user_id": 1, "nickname": "pepe"}`,
+	})
+
+	s := &usersService{}
+
+	user, err := s.GetUser(1, "a1b2c3d4e5")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.EqualValues(t, "pepe", user.Nickname)
+}
