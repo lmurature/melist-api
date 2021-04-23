437f8065bf0746fd304acda672f0c2a11101b6cc
lmurature <lucio.murature@mercadolibre.com>
2021-04-23 20:14:27 -0300

    env variables to connect db

--- src/api/clients/database/mysql_client.go
+++ src/api/clients/database/mysql_client.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"github.com/lmurature/melist-api/src/api/config"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -13,7 +15,7 @@ var (
 
 func init() {
 	var err error
-	DbClient, err = sql.Open("mysql", "root:root@/melist")
+	DbClient, err = sql.Open("mysql", fmt.Sprintf("%s:%s@%s/melist", config.DbUser, config.DbPass, config.DbHost))
 	if err != nil {
 		panic(err)
 	}
--- src/api/config/config.go
+++ src/api/config/config.go
@@ -9,14 +9,24 @@ var (
 	RedirectUri string
 	SecretKey string
 
+	DbUser string
+	DbPass string
+	DbHost string
+
 	DbDateLayout = "2006-01-02 15:04:05"
 )
 
 func init() {
 	if !isDevelopment() {
 		RedirectUri = "http://melist-app.herokuapp.com/auth/authorized"
+		DbUser = os.Getenv("DB_USER")
+		DbPass = os.Getenv("DB_PASS")
+		DbHost = os.Getenv("DB_HOST")
 	} else {
 		RedirectUri = "http://localhost:3000/auth/authorized"
+		DbUser = "root"
+		DbPass = "root"
+		DbHost = ""
 	}
 
 	SecretKey = os.Getenv("SECRET_KEY")
